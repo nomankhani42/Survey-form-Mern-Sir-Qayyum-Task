3fd0b162f883e35b30ff68fd6df2f3dc87ffadba
Noman Khan <khaninoan0@gmail.com>
2024-09-27 05:19:00 -0700

    added frontend in backend

--- App.js
+++ App.js
@@ -3,13 +3,23 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import  FormRouter from './Routes/Form.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
 
 
 dotenv.config();
 const App=express();
 App.use(cors());
+App.use(express.static(path.join(__dirname,'./build')))
 
 App.use('/api/v1',FormRouter)
+App.use('*',(req,res)=>{
+  return res.sendFile(path.join(__dirname,'/build','index.html'))
+})
+
 
 // db connection 
 
